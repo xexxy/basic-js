c342d2a865f5267ac419fa8d705b3c525481fde3
xexxy <38164075+xexxy@users.noreply.github.com>
2020-10-31 01:02:11 +0300

    feat: add count-cats functionality

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// module.exports = function countCats(/* matrix */) {
+//   throw new CustomError('Not implemented');
+//   // remove line with error and write your code here
+// };
+
+// module.exports = function countCats(matrix) {
+
+//   let number = matrix.flat(Infinity).join('').match(/\^\^/g) || [];
+//   // let number = matrix.flat(Infinity).join('').replace(/\s/g, '').match(/\^\^/g) || [];
+
+//   return number.length;
+// };
+
+module.exports = function countCats(matrix) {
+
+  let number = 0,
+    array = matrix.flat(Infinity) || [];
+  for (item of array) {
+    if (/^\^\^$/.test(item)) {
+      number++;
+    }
+  }
+
+  return number;
 };
