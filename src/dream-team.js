897ffb094670dba80890a434523c016c148d4ee5
xexxy <38164075+xexxy@users.noreply.github.com>
2020-10-31 16:17:23 +0300

    feat: add dream-team functionality

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let symbols = [];
+
+  if (Array.isArray(members)) {
+    for (let member of members) {
+      if (typeof(member) == 'string') {
+        symbols.push(member.toUpperCase().match(/[A-Za-z]/));
+      } else {
+        continue;
+      }
+    }
+  } else {
+    return false;
+  }
+
+  return symbols.sort().join('');
 };
